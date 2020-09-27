82889f736c0086d86db92250b0216aff2bb45969
Roberto Santalla <roobre@roobre.es>
2020-09-27 19:09:01 +0200

    http: use $PORT if present

--- http/main.go
+++ http/main.go
@@ -11,6 +11,9 @@ func main() {
 	if len(os.Args) >= 2 {
 		addr = os.Args[1]
 	}
+	if port := os.Getenv("PORT"); port != "" {
+		addr = ":" + port
+	}
 
 	h := NewHTTPApi()
 	log.Println("Listening on " + addr)
