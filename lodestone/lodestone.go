b31a9a2cca863a26b7743680de2b2f28441c082c
Roberto Santalla <roobre@roobre.es>
2020-10-03 21:28:58 +0200

    lodestone: use time-based retry mechanism

--- lodestone/lodestone.go
+++ lodestone/lodestone.go
@@ -22,6 +22,8 @@ func (lhe HTTPError) Error() string {
 	return fmt.Sprintf("lodestone returned status %d %s", lhe, http.StatusText(int(lhe)))
 }
 
+const LodestoneHTTPTimeout = 20 * time.Second
+
 // HTTPClient uses an http.Client to connec to to the Lodestone and retrieve data
 type HTTPClient struct {
 	Region     string
@@ -42,6 +44,7 @@ func (hlp *HTTPClient) Request(query string) (io.ReadCloser, error) {
 
 	var response *http.Response
 	try := 1
+	start := time.Now()
 	for {
 		response, err = hlp.HTTPClient.Do(request)
 		if err != nil { // Request failed hard, return error
@@ -49,16 +52,21 @@ func (hlp *HTTPClient) Request(query string) (io.ReadCloser, error) {
 		}
 
 		// If TooManyRequests and retries left, sleep then continue
-		if try <= 10 && response.StatusCode == http.StatusTooManyRequests {
-			// Linear backoff, wait between n and n+3 seconds where n is the attempt number
-			retry := time.Duration(1+rand.Intn(try+2)) * time.Second
-			log.Printf("Lodestone ratelimit hit, retrying in %fs", retry.Seconds())
-			time.Sleep(retry)
-			try++
-			continue
+		if response.StatusCode == http.StatusTooManyRequests {
+			elapsed := time.Since(start)
+			if elapsed < LodestoneHTTPTimeout {
+				// Linear backoff, wait between n and n+3 seconds where n is the attempt number
+				retry := time.Duration(1+rand.Intn(try+2)) * time.Second
+				log.Printf("Lodestone ratelimit hit, retrying in %fs", retry.Seconds())
+				time.Sleep(retry)
+				try++
+				continue
+			} else {
+				log.Printf("%.0fs passed since first attempt, giving up", elapsed.Seconds())
+			}
 		}
 
-		// Out of retires or other non-ok status, return error
+		// Timeout or other non-ok status, return error
 		if response.StatusCode != http.StatusOK {
 			return nil, HTTPError(response.StatusCode)
 		}
